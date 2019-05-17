27b1620fcbbbd8b2fb0920d6107ab884f36c7be1
jacksonderock <jackson.derock@cpe.fr>
2019-05-17 17:44:49 +0200

    Inventory add & delete

---
+++ micro-services/inventory/src/main/java/fr/cardgame/inventory/service/InventoryService.java
@@ -0,0 +1,29 @@
+package fr.cardgame.inventory.service;
+
+import fr.cardgame.inventory.dto.AddCardDto;
+import fr.cardgame.inventory.dto.DeleteCardDto;
+import fr.cardgame.inventory.factory.InventoryFactory;
+import fr.cardgame.inventory.model.Inventory;
+import fr.cardgame.inventory.repository.InventoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InventoryService {
+
+    @Autowired
+    private InventoryRepository inventoryRepository;
+
+    @Autowired
+    private InventoryFactory inventoryFactory;
+
+    public Inventory addCardInventory(AddCardDto addCardDto) {
+        Inventory inventory = this.inventoryFactory.createInventory(addCardDto);
+        this.inventoryRepository.save(inventory);
+        return inventory;
+    }
+
+    public void deleteCardInventory(DeleteCardDto deleteCardDto){
+        this.inventoryRepository.delete(deleteCardDto.getId());
+    }
+}
