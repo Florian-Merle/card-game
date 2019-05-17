01cacbe11e76416900ea1664536d1928526ef495
florian <florian.david.merle@gmail.com>
2019-05-17 17:37:06 +0200

    Add reverse proxy micro service

---
+++ micro-services/reverse-proxy/src/main/java/fr/cardgame/RestJpaMysqlSpringBootSampleApplication.java
@@ -0,0 +1,12 @@
+package fr.cardgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestJpaMysqlSpringBootSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestJpaMysqlSpringBootSampleApplication.class, args);
+	}
+}
