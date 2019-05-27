eb138dc20f2ef09091582745919c641e52b699f3
jacksonderock <jackson.derock@cpe.fr>
2019-05-27 11:09:39 +0200

    Get inventory

--- micro-services/inventory/src/main/java/fr/cardgame/inventory/repository/InventoryRepository.java
+++ micro-services/inventory/src/main/java/fr/cardgame/inventory/repository/InventoryRepository.java
@@ -14,4 +14,6 @@ import org.springframework.data.repository.CrudRepository;
  */
 public interface InventoryRepository extends CrudRepository<Inventory, Integer> {
 
+    public Iterable<Inventory> findByIdUser(Integer idUser);
+
 }
