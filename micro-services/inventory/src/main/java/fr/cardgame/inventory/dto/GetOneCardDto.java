9461b5665425fdc598e5a06229b2931f9c05a1c1
jacksonderock <jackson.derock@cpe.fr>
2019-05-27 16:16:00 +0200

    Inventory - Rajout méthode pour récupérer une carte par son id

---
+++ micro-services/inventory/src/main/java/fr/cardgame/inventory/dto/GetOneCardDto.java
@@ -0,0 +1,17 @@
+package fr.cardgame.inventory.dto;
+
+public class GetOneCardDto {
+
+    private Integer id;
+
+    public GetOneCardDto() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
