04e54aaec6d70811bb42ec1c9e3f1a8905127cd5
jacksonderock <jackson.derock@cpe.fr>
2019-05-27 11:48:36 +0200

    Update inventory energy

--- micro-services/inventory/src/main/java/fr/cardgame/inventory/controller/InventoryRestController.java
+++ micro-services/inventory/src/main/java/fr/cardgame/inventory/controller/InventoryRestController.java
@@ -3,6 +3,7 @@ package fr.cardgame.inventory.controller;
 import fr.cardgame.inventory.dto.AddCardDto;
 import fr.cardgame.inventory.dto.DeleteCardDto;
 import fr.cardgame.inventory.dto.GetCardDto;
+import fr.cardgame.inventory.dto.UpdateCardDto;
 import fr.cardgame.inventory.model.Inventory;
 import fr.cardgame.inventory.service.InventoryService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,6 +20,7 @@ public class InventoryRestController {
 	@Autowired
 	private InventoryService inventoryService;
 
+	//ajouter une carte à l'inventaire
 	@RequestMapping(method = RequestMethod.POST, value = "/addCardInventory")
 	private ResponseEntity addCardInventory(@RequestBody AddCardDto addCardDto){
 		Inventory inventory = this.inventoryService.addCardInventory(addCardDto);
@@ -26,15 +28,24 @@ public class InventoryRestController {
 		return new ResponseEntity<Inventory>(inventory, HttpStatus.OK);
 	}
 
+	//supprimer une carte de l'inventaire
 	@RequestMapping(method = RequestMethod.DELETE, value = "/deleteCardInventory")
 	private ResponseEntity deleteInventoryCard(@RequestBody DeleteCardDto deleteCardDto){
 		this.inventoryService.deleteCardInventory(deleteCardDto);
 		return new ResponseEntity(HttpStatus.OK);
 	}
 
+	//récupérer une carte de l'inventaire
 	@RequestMapping(method = RequestMethod.GET, value = "/getCardInventory")
 	private ResponseEntity getInventoryCard(@RequestBody GetCardDto getCardDto){
 		Iterable<Inventory> cardInventory = this.inventoryService.getCardInventory(getCardDto);
 		return new ResponseEntity<Iterable<Inventory>>(cardInventory, HttpStatus.OK);
+	}
+
+	//mettre à jour l'energie d'une carte de l'inventaire
+	@RequestMapping(method = RequestMethod.PUT, value = "/updateCardInventory")
+	private ResponseEntity updateInventoryCard(@RequestBody UpdateCardDto updateCardDto){
+		Inventory inventory = this.inventoryService.updateCardInventory(updateCardDto);
+		return new ResponseEntity<Inventory>(inventory, HttpStatus.OK);
 	}
 }
--- micro-services/inventory/src/main/java/fr/cardgame/inventory/service/InventoryService.java
+++ micro-services/inventory/src/main/java/fr/cardgame/inventory/service/InventoryService.java
@@ -3,6 +3,7 @@ package fr.cardgame.inventory.service;
 import fr.cardgame.inventory.dto.AddCardDto;
 import fr.cardgame.inventory.dto.DeleteCardDto;
 import fr.cardgame.inventory.dto.GetCardDto;
+import fr.cardgame.inventory.dto.UpdateCardDto;
 import fr.cardgame.inventory.factory.InventoryFactory;
 import fr.cardgame.inventory.model.Inventory;
 import fr.cardgame.inventory.repository.InventoryRepository;
@@ -18,18 +19,29 @@ public class InventoryService {
     @Autowired
     private InventoryFactory inventoryFactory;
 
+    //ajouter une carte dans l'inventaire
     public Inventory addCardInventory(AddCardDto addCardDto) {
         Inventory inventory = this.inventoryFactory.createInventory(addCardDto);
         this.inventoryRepository.save(inventory);
         return inventory;
     }
 
+    //supprimer une carte de l'inventaire
     public void deleteCardInventory(DeleteCardDto deleteCardDto){
         this.inventoryRepository.delete(deleteCardDto.getId());
     }
 
+    //récupérer une carte de l'inventaire
     public Iterable<Inventory> getCardInventory(GetCardDto getCardDto){
 
         return this.inventoryRepository.findByIdUser(getCardDto.getIdUser());
+    }
+
+    //mettre à jour l'énergie d'une carte de l'inventaire
+    public Inventory updateCardInventory(UpdateCardDto updateCardDto){
+        Inventory inventory = this.inventoryRepository.findById(updateCardDto.getIdCard());
+        inventory.setEnergy(updateCardDto.getEnergy());
+        this.inventoryRepository.save(inventory);
+        return inventory;
     }
 }
