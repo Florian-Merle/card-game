654772652a27bdcef28ef9163e6facde2798df2b
p1500361 <alexandre.boulet@etu.univ-lyon1.fr>
2019-05-28 09:02:14 +0200

    init shop functionnal test

---
+++ micro-services/shop/src/test/java/fr/cardgame/shop/ShopMicroServiceTests.java
@@ -0,0 +1,23 @@
+package fr.cardgame.shop;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShopMicroServiceTests {
+
+
+}
